c5aaf49598e287cf90dac8e3423707609813801f
thelostsoul80-wq <thelostsoul80@gmail.com>
2026-03-25 13:59:24 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
-const CACHE_NAME = 'splice-cache-v1';
-const urlsToCache = ['/', '/index.html', '/logo2.png'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
 });
